b31bf3a2a6dfb2a2565a43e44efec616131641cd
lee-1980 <ljsitsolution@gmail.com>
2023-09-27 02:13:11 +0800

    fixed the javascript enabled issue

--- index.js
+++ index.js
@@ -17,21 +17,24 @@ dotenv.config();
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-console.log(__dirname)
+
 
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
-app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
 
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/home", propertyRouter);
 app.use("/api/v1/Febest", febestRouter);
 app.use("/api/v1/Autoplus", autoplusRouter);
 app.use("/api/v1/logs", LogsRouter);
+
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+
 
 const startServer = async () => {
     try {
